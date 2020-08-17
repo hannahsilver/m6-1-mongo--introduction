d72a756afae1f172fc919e50ad2ee6d8833d2489
Hannah Silver <hannah_silver@hotmail.com>
2020-08-17 17:21:25 -0400

    exercise 2.4

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -45,4 +45,35 @@ const getGreeting = async (req, res) => {
   });
 };
 
-module.exports = { createGreeting, getGreeting };
+const getGreetings = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  const db = client.db("exercise_1");
+
+  const greetings = await db.collection("greetings").find().toArray();
+
+  let start = 0;
+  let limit = 24;
+
+  if (req.query.start !== undefined) {
+    start = Number(req.query.start);
+  }
+
+  if (req.query.limit !== undefined) {
+    limit = Number(req.query.limit);
+  }
+
+  if (greetings.length > 0) {
+    res
+      .status(200)
+      .json({ status: 200, data: greetings.slice(start, start + limit) });
+  } else {
+    res.status(400).json({ status: 404, data: "Greetings Not Found" });
+  }
+
+  client.close();
+};
+
+module.exports = { createGreeting, getGreeting, getGreetings };
